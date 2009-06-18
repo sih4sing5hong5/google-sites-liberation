4a5d81fca0d9635eea7da2dc3a5517411e7b00b3
Ben Simon <yourfriendben@localhost>
2009-06-18 14:27:44 -0500

    Fixed bug in InMemoryEntryStore
    Made sure that getChildrenIds() and getEntryIds() return the empty
    ImmutableSet when necessary

--- src/main/java/com/google/sites/liberation/InMemoryEntryStore.java
+++ src/main/java/com/google/sites/liberation/InMemoryEntryStore.java
@@ -1,3 +1,19 @@
+/*
+ * Copyright (C) 2009 Google Inc.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
 package com.google.sites.liberation;
 
 import com.google.common.collect.ImmutableSet;
@@ -27,6 +43,8 @@ public class InMemoryEntryStore implements EntryStore {
   
   @Override
   public ImmutableSet<String> getChildrenIds(String parentId, EntryType type) {
+    if (children.get(parentId)==null || children.get(parentId).get(type)==null)
+      return ImmutableSet.of();
     return ImmutableSet.copyOf(children.get(parentId).get(type));
   }
 
@@ -37,6 +55,8 @@ public class InMemoryEntryStore implements EntryStore {
 
   @Override
   public ImmutableSet<String> getEntryIds(EntryType type) {
+    if (ids.get(type) == null)
+      return ImmutableSet.of();
     return ImmutableSet.copyOf(ids.get(type));
   }
 
