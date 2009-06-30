ca664fa981e786feaa81f709134af290f0dcadcd
Ben Simon <yourfriendben@localhost>
2009-06-30 17:29:35 -0500

    Added EntryStore, InMemoryEntryStore, and test

---
+++ src/main/java/com/google/sites/liberation/InMemoryEntryStore.java
@@ -0,0 +1,71 @@
+package com.google.sites.liberation;
+
+import com.google.gdata.data.ILink;
+import com.google.gdata.data.Link;
+import com.google.gdata.data.sites.BaseContentEntry;
+import com.google.gdata.data.sites.SitesLink;
+import com.google.common.base.Preconditions;
+import com.google.common.collect.Multimap;
+import com.google.common.collect.Maps;
+import com.google.common.collect.HashMultimap;
+
+import java.util.Collection;
+import java.util.Map;
+
+/**
+ * This class is an in-memory implementation of {@link EntryStore}.
+ * 
+ * @author bsimon@google.com (Benjamin Simon)
+ */
+final class InMemoryEntryStore implements EntryStore {
+
+  private final Map<String, BaseContentEntry<?>> entries;
+  private final Multimap<String, BaseContentEntry<?>> children;
+  
+  /**
+   * Creates a new InMemoryEntryStore which provides constant time storage 
+   * and retrieval of entries by id or parent id.
+   */
+  public InMemoryEntryStore() {
+    entries = Maps.newHashMap();
+    children = HashMultimap.create();
+  }
+  
+  /**
+   * Stores the given content entry.
+   */
+  @Override
+  public void addEntry(BaseContentEntry<?> entry) {
+    Preconditions.checkNotNull(entry);
+    String id = entry.getId();
+    Preconditions.checkArgument(id != null && entries.get(id) == null,
+        "All entries must have a unique non-null id!");
+    entries.put(entry.getId(), entry);
+    Link parentLink = entry.getLink(SitesLink.Rel.PARENT, ILink.Type.ATOM);
+    if (parentLink != null) {
+      String parentId = parentLink.getHref();
+      children.put(parentId, entry);
+    }
+  }
+  
+  /**
+   * Returns a collection containing all entries with parent specified by the 
+   * given {@code id}.
+   */
+  @Override
+  public Collection<BaseContentEntry<?>> getChildren(String id) {
+    Preconditions.checkNotNull(id);
+    return children.get(id);
+  }
+
+  /**
+  * Retrieves the entry with the given {@code id} or {@code null} if there is 
+  * no such entry.
+  */
+  @Override
+  public BaseContentEntry<?> getEntry(String id) {
+    Preconditions.checkNotNull(id);
+    return entries.get(id);
+  }
+
+}
